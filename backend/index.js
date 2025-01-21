1bebc5569eab8e02f7f2547c482048613b61539b
HIMANSHU PRAKASH MISHRA <mhimanshuprakash@gmail.com>
2025-01-21 09:59:53 +0530

    committed

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const env = require("dotenv");
 const connectDatabase = require("./config/database.js");
+const userRoutes = require("./Routes/userRoutes.js");
 
 env.config();
 const app = express();
 
 connectDatabase();
 
-app.listen(process.env.BACKEND_PATH,()=>{
+app.use("/api/users", userRoutes);
+
+
+
+app.listen(process.env.BACKEND_PATH || 3000, ()=>{
     console.log(`listening on port ${process.env.BACKEND_PATH}`);
     
 })
